babf82b82c53e4cc0a1634dd6ddafbf11afe1a5a
leekilhun <pba7189@gmail.com>
2022-09-14 09:52:25 +0900

    ver 0

---
+++ jL_loader/src/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2022. 9. 13.
+ *      Author: gns2l
+ *
+ *
+ * jL_downloader PC실행 파일
+ * 강제종료  Ctrl +C
+ *
+ */
+
+#include "main.h"
+
+void exitISR(int sig)
+{
+  apExit();
+}
+
+
+int main(int argc, char *argv[])
+{
+  signal(SIGINT, exitISR);
+  setbuf(stdout, NULL);
+
+  sysInit();
+  hwInit();
+  apInit();
+
+  apMain(argc, argv);
+
+  return 0;
+}
+
